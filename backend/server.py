afcc9ef8587ba50e0bf6aedd87506a9e87976248
Manaal Khan <manaalkhan@manaals-mbp.dyndns.rice.edu>
2022-09-14 15:08:22 -0500

    initial backend

---
+++ backend/server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from pymongo import MongoClient
+import pymongo
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv('.env')
+
+CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
+
+def get_database():
+    client = MongoClient(CONNECTION_STRING)
+    return client['test1']
+
+@app.route('/getData')
+def get_data():
+    dbname = get_database()
+    print(dbname)
+    # Create a new collection
+    # collection_name = dbname['TestData']
+    collection_name = dbname['audrey']
+    print(collection_name)
+    jsn = []
+    item_details = collection_name.find()
+    print(item_details, ' item')
+    for item in item_details:
+        # This does not give a very readable output
+        jsn.append(item)
+    return jsn
+
+# get_data()
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()

915a5b7eaebb13911e3adb06efecf2b22a298fbb
Manaal Khan <ikhanz2000@gmail.com>
2022-09-14 19:38:26 -0500

    send JSON to frontend

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,8 @@ from pymongo import MongoClient
 import pymongo
 import os
 from dotenv import load_dotenv
+import json
+import certifi
 
 app = Flask(__name__)
 CORS(app) # Allow Cross-Origin Resource Sharing
@@ -13,25 +15,24 @@ load_dotenv('.env')
 CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
 
 def get_database():
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
     return client['crwd']
 
 @app.route('/getData')
 def get_data():
-    dbname = get_database()
-    print(dbname)
-    # Create a new collection
-    collection_name = dbname['TestData']
-    print(collection_name)
-    jsn = []
-    item_details = collection_name.find()
-    print(item_details, ' item')
-    for item in item_details:
-        # This does not give a very readable output
-        jsn.append(item)
-    return jsn
-
-# get_data()
+    db = get_database()
+    # get the collection
+    collection = db.get_collection("TestData")
+    documents = collection.find()
+    jsns = [] # list of json objects representing the documents
+    for i in range(3):
+        for document in documents:
+            jsn = {}        
+            for key in document:
+                if key != '_id':
+                    jsn[key] = document[key]
+            jsns.append(jsn)
+    return jsns
 
 @app.route('/')
 def hello():

db29d4211bf7505a4dfa18fc6a8ef080d0ee2ea5
Ryan Knightly <reknightly@gmail.com>
2022-09-19 16:59:48 -0500

    Replace MongoDB fetch with hard-coded data

--- backend/server.py
+++ backend/server.py
@@ -1,47 +1,28 @@
 from typing import List, Dict
 from flask import Flask
 from flask_cors import CORS
-from pymongo import MongoClient
-import pymongo
-import os
-from dotenv import load_dotenv
-import json
-import certifi
 
 app = Flask(__name__)
 
  # Allow Cross-Origin Resource Sharing
 CORS(app)
 
-# Load the Database URI from .env
-load_dotenv('.env')
-DATABASE_URI = os.environ.get("CONNECTION_STRING")
-
-def get_database():
-    tlsCAFile = certifi.where()
-    print(f"[INFO] Using TLS CA File: {tlsCAFile}")
-    client = MongoClient(DATABASE_URI, tlsCAFile=tlsCAFile)
-    return client['crwd']
-
 @app.route('/getData')
-def get_data():
-    # Check for the database uri
-    if DATABASE_URI is None:
-        print(f"[ERROR] No connection string!")
-        return []
-    db = get_database()
-    # Get the collection
-    collection = db.get_collection("TestData")
-    documents = collection.find()
-    # Convert each document to JSON
-    json_documents: List[Dict] = []
-    for document in documents:
-        jsn = {}        
-        for key in document:
-            if key != '_id':
-                jsn[key] = document[key]
-        json_documents.append(jsn)
-    return json_documents
+def get_data() -> List[Dict]:
+    return [
+        {
+            'time': '19/09/2022 16:32:32',
+            'nDevices': 0
+        },
+        {
+            'time': '19/09/2022 16:37:32',
+            'nDevices': 5
+        },
+        {
+            'time': '19/09/2022 16:42:32',
+            'nDevices': 9
+        }
+    ]
 
 @app.route('/')
 def hello():

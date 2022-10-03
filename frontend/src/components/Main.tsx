6c6b496292c4378095e07eabcf6c1fa1ac874906
Daphne Yang <dy27@rice.edu>
2022-10-03 10:44:50 -0500

    main page now shows message from /getCurrentCrowd

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 import {
   Link
 } from "react-router-dom";
 
 function Main() {
-    return (
-      <>
+
+  // Store server response
+  const [crowdResponse, setCrowdResponse] = useState("N/A");
+
+  // Fetch / from server
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/getCurrentCrowd")
+    .then((response) => response.text())
+    .then((responseText) => {
+      setCrowdResponse(responseText);
+    });
+  }, [])
+
+  return (
+    <div className="Main">
       <div>
         <h2>Main Page</h2>
       </div>
@@ -19,8 +33,12 @@ function Main() {
           </li>
         </ul>
       </div>
-      </>
-    );
-  }
+        <p>Response from server (127.0.0.1:5000/getCurrentCrowd):</p>
+        <pre id="GFG_DOWN" style={{"color":"Red", "fontSize":"16px"}}>
+          {crowdResponse}
+        </pre>
+    </div>
+  );
+}
 
 export default Main;

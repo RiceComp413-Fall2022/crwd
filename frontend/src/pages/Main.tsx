8ad484a876b134b17b6f2cad1b51e6604031efc0
Ryan Knightly <reknightly@gmail.com>
2022-10-04 11:09:00 -0500

    Move status box from side to top

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import StatusBox from '../components/StatusBox';
 import Graph from '../components/Graph';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 import './Main.css';
 
@@ -9,20 +9,19 @@ function Main() {
 
   return (
     <div className="main">
-      <div>
+      
+      <Row>
         <h1 className="title">crwd.io</h1>
-      </div>
-
-      <Row className="align-items-center">
-        {/* Left half: Graph */}
-        <Col className="col-9">
-          <Graph/>
-        </Col>
-        {/* Right half: Status */}
-        <Col className="col-3">
-          <StatusBox/>
-        </Col>
       </Row>
+
+      <Row>
+        <StatusBox/>
+      </Row>
+
+      <Row>
+        <Graph/>
+      </Row>
+
     </div>
   );
 }

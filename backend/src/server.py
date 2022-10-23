42261d7f79d9a35555dbde06dd9c5467eb79a731
Ryan Knightly <reknightly@gmail.com>
2022-10-23 15:16:24 -0500

    Use central timezone, add csv endpoint

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,7 +1,11 @@
 from typing import List, Dict, Tuple
+
 from flask import Flask
 from flask_cors import CORS
-import datetime
+from dotenv import load_dotenv
+from datetime import date, datetime, timezone, timedelta
+from pytz import timezone
+import os
 
 from src.service import Service
 
@@ -13,22 +17,33 @@ app = Flask(__name__)
  # Allow Cross-Origin Resource Sharing
 CORS(app)
 
+# Read .env file
+load_dotenv()
+
+# Read PASSKEY from environment
+PASSKEY = os.getenv('PASSKEY')
+if PASSKEY is None:
+    print('No PASSKEY defined - check .env file. Exiting.')
+    quit()
+
+# Use US central timezone
+TIMEZONE = timezone('US/Central')
 
 # Create Service instance
-date = datetime.date.today()
-opening_time = datetime.datetime(date.year, date.month, date.day, 8, 0, 0)
-service_obj = Service(opening_time, BACKUP_CSV_PATH)
+date = date.today()
+opening_time = datetime(date.year, date.month, date.day, 8, 0, 0)
+service_obj = Service(opening_time, BACKUP_CSV_PATH, PASSKEY, TIMEZONE)
 service_obj.restore_from_csv()  # Restore from backup
-
-
-@app.route('/getDummyData')
-def get_data_route() -> List[Tuple[str, int]]:
-    return service_obj.get_dummy_data()
 
 
 @app.route('/')
 def hello() -> str:
     return 'Hello, World!'
+
+
+@app.route('/getDummyData')
+def get_data_route() -> List[Tuple[str, int]]:
+    return service_obj.get_dummy_data()
 
 
 @app.route('/getCurrentStatus')
@@ -41,7 +56,13 @@ def get_all_data_route() -> Dict:
     return service_obj.get_all_data()
 
 
-@app.route('/updateTotalDevicesCompromised/<numDevices>')
+@app.route('/getAllDataCSV')
+def get_all_data_csv_route() -> str:
+    return service_obj.get_csv_browser()
+
+
+# TODO: remove this endpoint. For development / testing only.
+@app.route('/updateTotalDevices/<numDevices>')
 def comp_update_total_devices_route(numDevices) -> str:
     service_obj.update_total_devices_comp(numDevices)
     return 'update succeeded'

c1aa95421120afee9b2f0096541bb392512ff23d
Ryan Knightly <reknightly@gmail.com>
2022-11-02 20:08:54 -0500

    Move MAX_CAPACITY to config

--- backend/src/service.py
+++ backend/src/service.py
@@ -12,7 +12,6 @@ from src import config
 
 class Service:
 
-    CHAUS_CAPACITY = 102
     STORED_DATE_FORMAT = '%m/%d/%Y %H:%M'
 
     def __init__(self, 
@@ -47,7 +46,7 @@ class Service:
         Creates a list of percentages of how busy Chaus was at every minute.
         '''
         datetime_to_perc = { 
-            date: round(count / self.CHAUS_CAPACITY * 100, 1) 
+            date: round(count / config.MAX_CAPACITY * 100, 1) 
             for date, count in self.data
         }
         return datetime_to_perc
@@ -57,20 +56,20 @@ class Service:
         Creates a list of percentages of how busy Chaus was at every minute from opening to now.
         '''
         today = datetime.now()
-        opening = config.oper_hours[today.weekday()][0]
-        closing = config.oper_hours[today.weekday()][1]
+        opening = config.OPEN_HOURS[today.weekday()][0]
+        closing = config.OPEN_HOURS[today.weekday()][1]
         datetime_to_perc = {}
         for date, count in self.data:
             formatted = datetime.strptime(date, '%m/%d/%Y %H:%M')
             if formatted.date() == today.date():
                 if formatted.time() >= opening.time() and formatted.time() <= closing.time():
-                    datetime_to_perc[date] = round(count / self.CHAUS_CAPACITY * 100, 1)
+                    datetime_to_perc[date] = round(count / config.MAX_CAPACITY * 100, 1)
         return datetime_to_perc
 
-    def chaus_is_open(self) -> bool:
+    def chaus_is_open(self) -> str:
         today = datetime.now()
-        opening = config.oper_hours[today.weekday()][0]
-        closing = config.oper_hours[today.weekday()][1]
+        opening = config.OPEN_HOURS[today.weekday()][0]
+        closing = config.OPEN_HOURS[today.weekday()][1]
         if today.time() >= opening.time() and today.time() <= closing.time():
             return 'true'
         return 'false'
@@ -93,7 +92,7 @@ class Service:
         current_time = datetime.now(self.timezone).replace(tzinfo=None)
         time_ago_message = timeago.format(last_update_time, current_time)
 
-        perc = round(count / self.CHAUS_CAPACITY * 100, 1)
+        perc = round(count / config.MAX_CAPACITY * 100, 1)
         if perc > 90:
             message = 'Chaus is super busy!'
         elif perc > 60:

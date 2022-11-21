eec5238158c546415e608fcd5e0feb8f042df496
Daphne Yang <dy27@rice.edu>
2022-11-20 18:30:17 -0600

    frontend status boxes for the 3 cafes

--- frontend/src/components/StatusBox.tsx
+++ frontend/src/components/StatusBox.tsx
@@ -5,8 +5,28 @@ import { BACKEND_URL } from '../Constants';
 import './StatusBox.css'
 
 
-// The expected data from the server's /getCurrentStatus endpoint
-interface Status {
+// The expected data from the server's /getCurrentStatus/chaus endpoint
+interface ChausStatus {
+  msg1: string;
+  msg2: string;
+  perc: number;
+  time: string;
+  backgroundColor: string;
+  textColor: string
+}
+
+// The expected data from the server's /getCurrentStatus/brochstein endpoint
+interface BrochsteinStatus {
+  msg1: string;
+  msg2: string;
+  perc: number;
+  time: string;
+  backgroundColor: string;
+  textColor: string
+}
+
+// The expected data from the server's /getCurrentStatus/audreys endpoint
+interface AudreysStatus {
   msg1: string;
   msg2: string;
   perc: number;
@@ -17,8 +37,28 @@ interface Status {
 
 function StatusBox() {
 
-  // Store server response
-  const [status, setStatus] = useState({
+  // Store server response for Chaus
+  const [chausStatus, setChausStatus] = useState({
+    msg1: 'N/A',
+    msg2: 'N/A',
+    perc: 0,
+    time: 'N/A', 
+    backgroundColor: 'transparent',
+    textColor: 'white'
+  });
+
+  // Store server response for Brochstein
+  const [brochsteinStatus, setBrochsteinStatus] = useState({
+    msg1: 'N/A',
+    msg2: 'N/A',
+    perc: 0,
+    time: 'N/A', 
+    backgroundColor: 'transparent',
+    textColor: 'white'
+  });
+
+  // Store server response for Audrey's
+  const [audreysStatus, setAudreysStatus] = useState({
     msg1: 'N/A',
     msg2: 'N/A',
     perc: 0,
@@ -28,12 +68,30 @@ function StatusBox() {
   });
 
 
-  // Fetch /getCurrentStatus from server
+  // Fetch /getCurrentStatus/chaus from server
   useEffect(() => {
-    fetch(BACKEND_URL + "/getCurrentStatus")
+    fetch(BACKEND_URL + "/getCurrentStatus/chaus")
       .then((response) => response.json())
-      .then((status: Status) => {
-        setStatus(status);
+      .then((chausStatus: ChausStatus) => {
+        setChausStatus(chausStatus);
+      });
+  }, [])
+
+  // Fetch /getCurrentStatus/brochstein from server
+  useEffect(() => {
+    fetch(BACKEND_URL + "/getCurrentStatus/brochstein")
+      .then((response) => response.json())
+      .then((brochsteinStatus: BrochsteinStatus) => {
+        setBrochsteinStatus(brochsteinStatus);
+      });
+  }, [])
+
+  // Fetch /getCurrentStatus/audreys from server
+  useEffect(() => {
+    fetch(BACKEND_URL + "/getCurrentStatus/audreys")
+      .then((response) => response.json())
+      .then((audreysStatus: AudreysStatus) => {
+        setAudreysStatus(audreysStatus);
       });
   }, [])
 
@@ -41,17 +99,28 @@ function StatusBox() {
   return (
     <Row>
       <Col md={true}>
-        <Card className={"textBox"} style={{backgroundColor: status.backgroundColor, color: status.textColor}}>
+        <Card className={"textBox"} style={{backgroundColor: chausStatus.backgroundColor, color: chausStatus.textColor}}>
           <Card.Body>
-            <h5><b>{status.msg1}</b></h5>
+            <h5><b>{chausStatus.msg1}</b></h5>
+            <h5><b>{chausStatus.msg2}</b></h5>
           </Card.Body>
         </Card>
       </Col>
       
       <Col md={true}>
-        <Card className={"textBox"} style={{backgroundColor: status.backgroundColor, color: status.textColor}}>
+        <Card className={"textBox"} style={{backgroundColor: brochsteinStatus.backgroundColor, color: brochsteinStatus.textColor}}>
           <Card.Body>
-            <h5><b>{status.msg2}</b></h5>
+            <h5><b>{brochsteinStatus.msg1}</b></h5>
+            <h5><b>{brochsteinStatus.msg2}</b></h5>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col md={true}>
+        <Card className={"textBox"} style={{backgroundColor: audreysStatus.backgroundColor, color: audreysStatus.textColor}}>
+          <Card.Body>
+            <h5><b>{audreysStatus.msg1}</b></h5>
+            <h5><b>{audreysStatus.msg2}</b></h5>
           </Card.Body>
         </Card>
       </Col>

317b71af6827b107607f30eab60e501ec6200daf
Manaal Khan <ikhanz2000@gmail.com>
2022-11-30 02:42:44 -0600

    updated audreys hours

--- backend/src/config.py
+++ backend/src/config.py
@@ -13,13 +13,13 @@ CHAUS_OPEN_HOURS = {0 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 23
               6 : (datetime(2022, 1, 1, 14, 0, 0), datetime(2022, 1, 1, 23, 59, 59))}
 
 # need to update
-AUDREYS_OPEN_HOURS = {0 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 23, 59, 59)),
-              1 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 23, 59, 59)),
-              2 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 23, 59, 59)),
-              3 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 23, 59, 59)),
-              4 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 17, 0, 0)),
-              5 : (datetime(2022, 1, 1, 9, 30, 0), datetime(2022, 1, 1, 17, 0, 0)),
-              6 : (datetime(2022, 1, 1, 14, 0, 0), datetime(2022, 1, 1, 23, 59, 59))}
+AUDREYS_OPEN_HOURS = {0 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 18, 0, 0)),
+              1 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 18, 0, 0)),
+              2 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 18, 0, 0)),
+              3 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 18, 0, 0)),
+              4 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 18, 0, 0)),
+              5 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 17, 0, 0)),
+              6 : (datetime(2022, 1, 1, 23, 59, 59), datetime(2022, 1, 1, 23, 59, 59))}
 
 BROCHSTEIN_OPEN_HOURS = {0 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 19, 0, 0)),
               1 : (datetime(2022, 1, 1, 7, 30, 0), datetime(2022, 1, 1, 19, 0, 0)),
